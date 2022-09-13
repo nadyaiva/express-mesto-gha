3f277aadabef2d334ec17eb794fadacc7163d659
nadyaiva <79001245+nadyaiva@users.noreply.github.com>
2022-09-13 23:20:34 +0200

    refactor: hendle errors for users

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,11 @@
 const User = require('../models/user');
+const {
+  WRONG_REQUEST_CODE, NOT_FOUND_CODE, serverRespondErr,
+} = require('../err');
 
 const getUsers = (req, res) => {
   User.find({}).then((users) => res.send(users))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(() => serverRespondErr());
 };
 
 const getUserById = (req, res) => {
@@ -14,21 +17,43 @@ const getUserById = (req, res) => {
 const createUser = (req, res) => {
   const { userName, about, avatar } = req.body;
   User.create({ userName, about, avatar }).then((user) => res.send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(WRONG_REQUEST_CODE).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+        return;
+      }
+      serverRespondErr(res);
+    });
 };
 
 const updateUser = (req, res) => {
   const { userName, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { userName, about })
     .then((user) => res.send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(WRONG_REQUEST_CODE).send({ massage: 'Переданы некорректные данные при обновлении профиля.' });
+      } else if (err.name === 'CastError') {
+        res.status(NOT_FOUND_CODE).send({ massage: 'Пользователь с указанным id не найден.' });
+      } else {
+        serverRespondErr(res);
+      }
+    });
 };
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar })
     .then((user) => res.send(user))
-    .catch((err) => res.status(500).send({ massage: err.massage }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(WRONG_REQUEST_CODE).send({ massage: 'Переданы некорректные данные при обновлении аватара.' });
+      } else if (err.name === 'CastError') {
+        res.status(NOT_FOUND_CODE).send({ massage: 'Пользователь с указанным id не найден.' });
+      } else {
+        serverRespondErr(res);
+      }
+    });
 };
 
 module.exports = {
