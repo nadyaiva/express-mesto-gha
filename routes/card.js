653b783b2cc61a7b0339b51dee738c8bc0e1a821
nadyaiva <79001245+nadyaiva@users.noreply.github.com>
2022-09-26 22:38:38 +0200

    refactor: add Joi to card router

--- routes/card.js
+++ routes/card.js
@@ -1,12 +1,36 @@
 const routerCard = require('express').Router();
+
+const { celebrate, Joi } = require('celebrate');
+
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 routerCard.get('/', getCards);
-routerCard.post('/', createCard);
-routerCard.delete('/:cardId', deleteCard);
-routerCard.put('/:cardId/likes', likeCard);
-routerCard.delete('/:cardId/likes', dislikeCard);
+
+routerCard.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().regex(/https?:\/\/[-/\w./~^:?#!@$&'()*+,;=\][]*/),
+  }),
+}), createCard);
+
+routerCard.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), deleteCard);
+
+routerCard.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), likeCard);
+
+routerCard.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), dislikeCard);
 
 module.exports = routerCard;
